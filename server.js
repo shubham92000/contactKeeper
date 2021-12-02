d886d80aa4c1814ee27c589489ed3c6559b58789
shubham92000 <sbhsingh616@gmail.com>
2021-12-02 20:30:44 +0530

    deployment

--- server.js
+++ server.js
@@ -1,24 +1,32 @@
 const express = require('express');  //import
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
 connectDB();
 
 //init middleware
-app.use(express.json({ extended : false }));
+app.use(express.json({ extended: false }));
 
 // app.get('/' , (req , res)=>{
 //   res.json({msg : 'Hello World'});
 // })
 
 //routes 
-app.use('/api/users' , require('./routes/users'));
-app.use('/api/auth' , require('./routes/auth'));
-app.use('/api/contacts' , require('./routes/contacts'));
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
+// serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT , () => {
+app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
